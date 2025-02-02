cebddcec335b95448a864e259cbaf6470e609c67
Tanya <dougtanyah@gmail.com>
2025-02-02 11:27:23 -0500

    feat: login method created

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,6 +5,28 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  // extract the username and passwork from the incoming login request
+  const {username, password} = req.body;
+  //check to see if the user already exists
+  const user = await User.findOne({
+    where: { username: username },
+  });
+  //if user does not exist, fail
+  if (!user) {
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
+  //if user exists validate by comparing it to what is in database
+  const passwordIsValid = await bcrypt.compare(password, user.password);
+  if (!passwordIsValid) {
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
+  //if user is valdated send a token (secret key cannot be null or undefined)
+  const secretKey = process.env.JWT_SECRET_KEY!;
+
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+
+  return res.json({ token });
+
 };
 
 const router = Router();
