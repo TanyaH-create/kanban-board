f9be577ff07178c8f6d120fac979b24bbbfcba9c
Tanya <dougtanyah@gmail.com>
2025-02-02 12:33:46 -0500

    feat: api route authentification added

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,20 @@
 import { Router } from 'express';
 import authRoutes from './auth-routes.js';
 import apiRoutes from './api/index.js';
+//import middleware function to authenticate token
 import { authenticateToken } from '../middleware/auth.js';
 
 const router = Router();
 
+//routes that handle the login and password validation.
 router.use('/auth', authRoutes);
+
 // TODO: Add authentication to the API routes
+//any routes that go through /api will require a valid token
+//from the user in the authorization header. If authenticateToken is successfull, then
+//apiRoutes will go through
+router.use('/api', authenticateToken, apiRoutes)
+
 router.use('/api', apiRoutes);
 
 export default router;
